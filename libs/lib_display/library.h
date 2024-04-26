20f84461b95805bbcb6c50a3d620a4de7a299c33
Vivien LEZIN <contact@vivien-lezin.fr>
2024-04-26 15:53:06 +0200

    feat: initial commit with project

---
+++ libs/lib_display/library.h
@@ -0,0 +1,6 @@
+#ifndef LIB_DISPLAY_LIBRARY_H
+#define LIB_DISPLAY_LIBRARY_H
+
+void hello(void);
+
+#endif //LIB_DISPLAY_LIBRARY_H
---
+++ libs/lib_model/library.h
@@ -0,0 +1,6 @@
+#ifndef LIB_MODEL_LIBRARY_H
+#define LIB_MODEL_LIBRARY_H
+
+void hello(void);
+
+#endif //LIB_MODEL_LIBRARY_H
