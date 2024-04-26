3d54367ddc223081c5903ded6ac664b2ee984492
Vivien LEZIN <contact@vivien-lezin.fr>
2024-04-26 18:15:20 +0200

    feat: :white_check_mark: add init list of ship :poop:

--- libs/lib_model/library.c
+++ libs/lib_model/library.c
@@ -2,8 +2,8 @@
 
 #include <stdlib.h>
 
-grid_case** grid_player;
-grid_case** grid_ordi;
+grid_case **grid_player;
+grid_case **grid_ordi;
 
 list_of_ships ships_player;
 list_of_ships ships_ordi;
@@ -33,18 +33,36 @@ void set_ship(int colonne, int row) {
     //todo : à implémenter
 }
 
+ship init_ship(const int size) {
+    ship ship;
+    ship.is_killed = false;
+    ship.coordinate = (coordinate *) malloc(size * sizeof(coordinate));
+    return ship;
+}
+
+list_of_ships init_list_of_ships(
+) {
+    list_of_ships list;
+    list.sm_ship = init_ship(SMALL_SHIP);
+    list.md_ship1 = init_ship(MEDIUM_SHIP);
+    list.md_ship2 = init_ship(MEDIUM_SHIP);
+    list.lg_ship = init_ship(LARGE_SHIP);
+    list.xl_ship = init_ship(XL_SHIP);
+    return list;
+}
+
 void init() {
-    grid_player = (grid_case**) malloc(GRID_SIZE*sizeof(grid_case));
-    if (grid_player==NULL){ exit(1); }
+    grid_player = (grid_case **) malloc(GRID_SIZE * sizeof(grid_case));
+    if (grid_player == NULL) { exit(1); }
     for (int i = 0; i < GRID_SIZE; ++i) {
-        grid_player[i] = (grid_case*) malloc(GRID_SIZE*sizeof(grid_case));
-        if (grid_player[i]==NULL){ exit(1); }
+        grid_player[i] = (grid_case *) malloc(GRID_SIZE * sizeof(grid_case));
+        if (grid_player[i] == NULL) { exit(1); }
     }
-    grid_ordi = (grid_case**) malloc(GRID_SIZE*sizeof(grid_case));
-    if (grid_ordi==NULL){ exit(1); }
+    grid_ordi = (grid_case **) malloc(GRID_SIZE * sizeof(grid_case));
+    if (grid_ordi == NULL) { exit(1); }
     for (int i = 0; i < GRID_SIZE; ++i) {
-        grid_ordi[i] = (grid_case*) malloc(GRID_SIZE*sizeof(grid_case));
-        if (grid_ordi[i]==NULL){ exit(1); }
+        grid_ordi[i] = (grid_case *) malloc(GRID_SIZE * sizeof(grid_case));
+        if (grid_ordi[i] == NULL) { exit(1); }
     }
 
 }
@@ -52,4 +70,16 @@ void init() {
 void end() {
     free(grid_player);
     free(grid_ordi);
+
+    free(ships_ordi.sm_ship.coordinate);
+    free(ships_ordi.md_ship1.coordinate);
+    free(ships_ordi.md_ship2.coordinate);
+    free(ships_ordi.lg_ship.coordinate);
+    free(ships_ordi.xl_ship.coordinate);
+
+    free(ships_player.sm_ship.coordinate);
+    free(ships_player.md_ship1.coordinate);
+    free(ships_player.md_ship2.coordinate);
+    free(ships_player.lg_ship.coordinate);
+    free(ships_player.xl_ship.coordinate);
 }
