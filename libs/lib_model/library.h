f1d00631685abf7dd0d1619f0f26b466d36e002f
Vivien LEZIN <contact@vivien-lezin.fr>
2024-04-26 16:52:36 +0200

    feat: modify the double negation :poop:

--- libs/lib_model/library.h
+++ libs/lib_model/library.h
@@ -1,12 +1,15 @@
 #ifndef LIB_MODEL_LIBRARY_H
 #define LIB_MODEL_LIBRARY_H
 #include <stdbool.h>
+
 #define SIZE 9
 
 struct __grid_case {
    bool is_fired;
-   bool is_empty;
+   bool is_contain_ship;
 };
 typedef struct __grid_case grid_case;
+
+
 
 #endif //LIB_MODEL_LIBRARY_H
