1b6459c44fbd2283959313e6b171b0607f91ef48
Vivien LEZIN <contact@vivien-lezin.fr>
2024-04-27 14:06:30 +0200

    feat: :art: separate in two methods the function print and add a clear method

--- app/main.c
+++ app/main.c
@@ -1,5 +1,6 @@
 // main.c
 #include <stdio.h>
+#include <stdlib.h>
 #include "../libs/lib_display/library.h"
 
 void init() {
@@ -34,11 +35,21 @@ int main() {
     gridEnemy[5][4].is_fired = true;
     gridEnemy[5][6].is_fired = true;
 
+
     printGrids(gridPlayer, gridEnemy);
 
     //💩 useless code -- just for waiting time
     int num;
     float fnum;
+    printf("Enter an integer and a floating-point number: ");
+    scanf("%d %f", &num, &fnum);
+    printf("You entered %d and %f\n", num, fnum);
+
+    // 👌 Usefull for clear console ("On windows") => use "clear" instead for linux env
+    system("cls");
+    printGrids(gridPlayer, gridEnemy);
+
+    //💩 useless code -- just for waiting time
     printf("Enter an integer and a floating-point number: ");
     scanf("%d %f", &num, &fnum);
     printf("You entered %d and %f\n", num, fnum);
--- libs/lib_display/library.c
+++ libs/lib_display/library.c
@@ -2,49 +2,57 @@
 #include "../lib_model/library.h"
 #include <stdio.h>
 
+void _printEnemyGrid(grid_case **gridEnemy, int i) {
+    for (int j = 0; j < GRID_SIZE; j++) {
+        if(gridEnemy[i][j].is_sank) {
+            printf("\033[31m"); // Rouge
+        }else{
+            if (gridEnemy[i][j].is_fired) {
+                if (gridEnemy[i][j].is_contain_ship) {
+                    printf("\033[33m"); // Jaune
+                } else {
+                    printf("\033[34m"); // Bleu
+                }
+            } else {
+                printf("\033[37m"); // Blanc
+            }
+        }
+        printf("%s ", (gridEnemy[i][j].is_fired || gridEnemy[i][j].is_sank ) && gridEnemy[i][j].is_contain_ship ? "#" : ".");
+    }
+}
+
+void _printPlayerGrid(grid_case **gridPlayer, int i) {
+    for (int j = 0; j < GRID_SIZE; j++) {
+        if(gridPlayer[i][j].is_sank) {
+            printf("\033[31m"); // Rouge
+        }else{
+            if (gridPlayer[i][j].is_fired) {
+                if (gridPlayer[i][j].is_contain_ship) {
+                    printf("\033[33m"); // Jaune
+                } else {
+                    printf("\033[34m"); // Bleu
+                }
+            } else {
+                if (gridPlayer[i][j].is_contain_ship) {
+                    printf("\033[32m"); // Vert
+                } else {
+                    printf("\033[37m"); // Blanc
+                }
+            }
+        }
+        printf("%s ", gridPlayer[i][j].is_contain_ship ? "#" : ".");
+    }
+}
+
 void printGrids(grid_case **gridPlayer, grid_case **gridEnemy) {
     printf("  1 2 3 4 5 6 7 8 9 10      1 2 3 4 5 6 7 8 9 10\n");
     char row = 'A';
     for (int i = 0; i < GRID_SIZE; i++) {
         printf("%c ", row++);
-        for (int j = 0; j < GRID_SIZE; j++) {
-            if(gridPlayer[i][j].is_sank) {
-                printf("\033[31m"); // Rouge
-            }else{
-                if (gridPlayer[i][j].is_fired) {
-                    if (gridPlayer[i][j].is_contain_ship) {
-                        printf("\033[33m"); // Jaune
-                    } else {
-                        printf("\033[34m"); // Bleu
-                    }
-                } else {
-                    if (gridPlayer[i][j].is_contain_ship) {
-                        printf("\033[32m"); // Vert
-                    } else {
-                        printf("\033[37m"); // Blanc
-                    }
-                }
-            }
-            printf("%s ", gridPlayer[i][j].is_contain_ship ? "#" : ".");
-        }
+        _printPlayerGrid(gridPlayer, i);
         printf("    "); // Ecart entre grilles
         printf("%c ", 'A' + i); // Colonne de lettres de grille tirs
-        for (int j = 0; j < GRID_SIZE; j++) {
-            if(gridEnemy[i][j].is_sank) {
-                printf("\033[31m"); // Rouge
-            }else{
-                if (gridEnemy[i][j].is_fired) {
-                    if (gridEnemy[i][j].is_contain_ship) {
-                        printf("\033[33m"); // Jaune
-                    } else {
-                        printf("\033[34m"); // Bleu
-                    }
-                } else {
-                    printf("\033[37m"); // Blanc
-                }
-            }
-            printf("%s ", (gridEnemy[i][j].is_fired || gridEnemy[i][j].is_sank ) && gridEnemy[i][j].is_contain_ship ? "#" : ".");
-        }
+        _printEnemyGrid(gridEnemy, i);
         printf("\n");
     }
 }
