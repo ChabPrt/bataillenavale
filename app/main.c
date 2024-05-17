aa7dbb282c6c2ccba2e651317bc8e858fe284e5c
ChabPrt <chabas.alexandre@gmail.com>
2024-05-17 14:19:33 +0200

    ⚗️

--- app/main.c
+++ app/main.c
@@ -21,45 +21,29 @@ int main() {
     init();
 
     grid_case **gridPlayer= get_grid_player();
-    /*gridPlayer[4][4].is_fired = true;
-    gridPlayer[5][5].is_fired = true;
-    gridPlayer[5][5].is_contain_ship = true;
-    gridPlayer[5][6].is_contain_ship = true;
-    gridPlayer[5][7].is_contain_ship = true;
-    gridPlayer[5][4].is_fired = true;
-    gridPlayer[5][6].is_fired = true;*/
-
     grid_case **gridEnemy= get_grid_ordi();
-    /*gridEnemy[4][4].is_fired = true;
-    gridEnemy[5][5].is_fired = true;
-    gridEnemy[5][5].is_contain_ship = true;
-    gridEnemy[5][6].is_contain_ship = true;
-    gridEnemy[5][7].is_contain_ship = true;
-    gridEnemy[2][3].is_sank = true;
-    gridEnemy[2][4].is_sank = true;
-    gridEnemy[2][5].is_sank = true;
-    gridEnemy[5][4].is_fired = true;
-    gridEnemy[5][6].is_fired = true;*/
 
+    while(true){
+        printGrids(gridPlayer, gridEnemy);
 
-    printGrids(gridPlayer, gridEnemy);
+        int colSpan;
+        printf("Sur quelle colonne portera votre tir ? ( A - J )");
+        scanf("%s", &colSpan);
 
-    //💩 useless code -- just for waiting time
-    int num;
-    float fnum;
-    printf("Enter an integer and a floating-point number: ");
-    scanf("%d %f", &num, &fnum);
-    printf("You entered %d and %f\n", num, fnum);
+        printf("Lecture : %d\n",colSpan);
 
-    // 👌 Usefull for clear console ("On windows") => use "clear" instead for linux env
-    system("cls");
-    printGrids(gridPlayer, gridEnemy);
+        if((65 <= colSpan && colSpan <= 74) || (97 <= colSpan && colSpan <= 106)){
+            printf("non");
+            break;
+        }
+        printf("oui");
+        break;
 
-    //💩 useless code -- just for waiting time
-    printf("Enter an integer and a floating-point number: ");
-    scanf("%d %f", &num, &fnum);
-    printf("You entered %d and %f\n", num, fnum);
+        // 👌 Usefull for clear console ("On windows") => use "clear" instead for linux env
+        system("cls");
+        printGrids(gridPlayer, gridEnemy);
+    }
 
+    while(true);
     end();
-    return 0;
 }
